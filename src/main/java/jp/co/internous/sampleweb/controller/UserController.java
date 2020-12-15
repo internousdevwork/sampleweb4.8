881bbc7f9d7d064e4abe889eda08f262e2b89096
k-kawamura-internous <k.kawamura@internous.co.jp>
2020-12-15 14:33:44 +0900

    不要なimport文を削除

--- src/main/java/jp/co/internous/sampleweb/controller/UserController.java
+++ src/main/java/jp/co/internous/sampleweb/controller/UserController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.gson.Gson;
-
 import jp.co.internous.sampleweb.model.domain.MstUser;
 import jp.co.internous.sampleweb.model.form.UserForm;
 import jp.co.internous.sampleweb.model.mapper.MstUserMapper;
