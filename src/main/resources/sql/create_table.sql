ddfa1d1d5c6ff48d8178e69ebc814bc09b5a08a3
k-kawamura-internous <k.kawamura@internous.co.jp>
2020-12-22 12:08:26 +0900

    テーブルの定義を設計書通りに修正

--- src/main/resources/sql/create_table.sql
+++ src/main/resources/sql/create_table.sql
@@ -1,33 +1,33 @@
 USE shopdb;
 
-CREATE TABLE mst_user (
- id int(11) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS mst_user (
+ id INT(11) PRIMARY KEY AUTO_INCREMENT,
  user_name VARCHAR(32) NOT NULL UNIQUE,
  password VARCHAR(16) NOT NULL,
  family_name VARCHAR(255) NOT NULL,
  first_name VARCHAR(255) NOT NULL,
  family_name_kana VARCHAR(255) NOT NULL,
  first_name_kana VARCHAR(255) NOT NULL,
- gender TINYINT DEFAULT 0,
+ gender TINYINT(1) DEFAULT 0,
  created_at TIMESTAMP NOT NULL default now(),
  updated_at TIMESTAMP NOT NULL default now()
 );
 
-CREATE TABLE mst_category (
- id int(11) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS mst_category (
+ id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
  category_name VARCHAR(255) NOT NULL,
  category_description VARCHAR(255),
  created_at TIMESTAMP NOT NULL default now(),
  updated_at TIMESTAMP NOT NULL default now()
 );
 
-CREATE TABLE mst_product (
- id int(11) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS mst_product (
+ id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
  product_name VARCHAR(255) NOT NULL UNIQUE,
  product_name_kana VARCHAR(255) NOT NULL UNIQUE,
  product_description VARCHAR(255),
- category_id int(11) NOT NULL,
- price int(11) NOT NULL,
+ category_id INT(11) NOT NULL,
+ price INT(11) NOT NULL,
  image_full_path VARCHAR(255) NOT NULL,
  release_date VARCHAR(10),
  release_company VARCHAR(255),
@@ -36,37 +36,37 @@ CREATE TABLE mst_product (
  FOREIGN KEY(category_id) REFERENCES mst_category(id)
 );
 
-CREATE TABLE mst_destination (
- id int(11) PRIMARY KEY AUTO_INCREMENT,
- user_id int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS mst_destination (
+ id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ user_id INT(11) NOT NULL,
  family_name VARCHAR(255) NOT NULL,
  first_name VARCHAR(255) NOT NULL,
  tel_number VARCHAR(13),
  address VARCHAR(255) NOT NULL,
- status TINYINT  NOT NULL DEFAULT 1,
+ status TINYINT(1) NOT NULL DEFAULT 1,
  created_at TIMESTAMP NOT NULL default now(),
  updated_at TIMESTAMP NOT NULL default now(),
  FOREIGN KEY(user_id) REFERENCES mst_user(id)
 );
 
-CREATE TABLE tbl_cart (
- id int(11) PRIMARY KEY AUTO_INCREMENT,
- user_id int(11) NOT NULL,
- product_id int(11) NOT NULL,
- product_count int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS tbl_cart (
+ id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ user_id INT(11) NOT NULL,
+ product_id INT(11) NOT NULL,
+ product_count INT(11) NOT NULL,
  created_at TIMESTAMP NOT NULL default now(),
  updated_at TIMESTAMP NOT NULL default now(),
  FOREIGN KEY(product_id) REFERENCES mst_product(id)
 );
 
-CREATE TABLE tbl_purchase_history (
- id int(11) PRIMARY KEY AUTO_INCREMENT,
- user_id int(11) NOT NULL,
- product_id int(11) NOT NULL,
- product_count int(11) NOT NULL,
- price int(11) NOT NULL,
- destination_id int(11) NOT NULL,
- status TINYINT  NOT NULL DEFAULT 1,
+CREATE TABLE IF NOT EXISTS tbl_purchase_history (
+ id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ user_id INT(11) NOT NULL,
+ product_id INT(11) NOT NULL,
+ product_count INT(11) NOT NULL,
+ price INT(11) NOT NULL,
+ destination_id INT(11) NOT NULL,
+ status TINYINT(1) NOT NULL DEFAULT 1,
  purchased_at TIMESTAMP NOT NULL default now(),
  created_at TIMESTAMP NOT NULL default now(),
  updated_at TIMESTAMP NOT NULL default now(),
