ec78b96de6beffc3689a28e856af0a1b3fad335b
k-kawamura-internous <k.kawamura@internous.co.jp>
2021-02-19 15:12:16 +0900

    文字化けしているコメントを削除

--- src/main/java/jp/co/internous/sampleweb/controller/AuthController.java
+++ src/main/java/jp/co/internous/sampleweb/controller/AuthController.java
@@ -27,45 +27,15 @@ public class AuthController {
 	
 	@Autowired
 	private LoginSession loginSession;
-	
+		
 	@RequestMapping("/login")
 	public String login(@RequestBody UserForm f) {
 		MstUser user = userMapper.findByUserNameAndPassword(f.getUserName(), f.getPassword());
 		
 		int tmpUserId = loginSession.getTmpUserId();
-		
 		// 仮IDでカート追加されていれば、本ユーザーIDに更新する。
 		if (user != null && tmpUserId != 0) {
-			//在DB裡面找到tempUserId的紀錄 
-			//新增商品到購物車的機能裡面，會有Insert Record To DB的動作，而且放的Id是TempUserId
-			
-			
-			// 沒有login的時候，我有三個紀錄
-			// 商品A, tempUserId;123
-			// 商品B, tempUserId;123
-			// 商品C, tempUserId;123
-			
-			//use tmpUserId find count in cart
 			int count = cartMapper.findCountByUserId(tmpUserId);
-		
-			// count == 3
-			// login之後
-			// real userId == 100
-			// cartMapper.updateUserId(user.getId(), tmpUserId);
-		    // 商品A, tempUserId;100
-			// 商品B, tempUserId;100
-			// 商品C, tempUserId;100
-			
-			
-			// if tempUserid == 0
-			// count == 0，找不到
-			
-			// if tempUserid != 0
-			// count == 0, 找不到
-			// count > 0, 找到紀錄了
-			
-			//如果有紀錄，我現在已經是Login
-			//所以要把這些logout之前放到購物車裡面的東西，變成是我的真正userId
 			if (count > 0) {
 				cartMapper.updateUserId(user.getId(), tmpUserId);
 			}
@@ -89,7 +59,7 @@ public class AuthController {
 	
 	@RequestMapping("/logout")
 	public String logout() {
-		loginSession.setTmpUserId(0); 
+		loginSession.setTmpUserId(0);
 		loginSession.setLogined(false);
 		loginSession.setUserId(0);
 		loginSession.setUserName(null);
@@ -97,7 +67,7 @@ public class AuthController {
 		
 		return "";
 	}
-	
+
 	@RequestMapping("/resetPassword")
 	public String resetPassword(@RequestBody UserForm f) {
 		String message = "パスワードが再設定されました。";
